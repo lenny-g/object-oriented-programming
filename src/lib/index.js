16d15de5816ea7c602496678f1171e6c5bfac994
lenny-g <leanne_gallag@live.co.uk>
2021-11-24 19:41:36 +0000

    updated css html and test

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,10 +1,63 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
 
-const writeToFile( )
+const mainQuestions = [
+    {
+       type: "list",
+       name: "role",
+       message: "What is your role?",
+          choices: [
+              { name: "Manager", value: "manager"},
+              { name: "Engineer", value: "engineer"},
+              { name: "Intern", value: "intern" }
+          ]
+      },
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+    {
+      type: "number",
+      name: "id",
+      message: "What is your ID? ",
+    },
+  
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email?",
+    }, ]
 
-const generateHTML() => {
-    map
-    // getRole 
-    
+
+managerQuestion = [{
+      
+    }]
+
+const generateHTML = (answers) => {
+    const {
+        role,
+        name,
+        id,
+        email,
+        officeNumber,
+        github,
+        school
+}
 }
 
+const writeToFile = (fileName, data) => {
+    fs.writeFile(fileName, data, (err) =>
+      err ? console.error(err) : console.log("Success!")
+    );
+};
+  
+
+const init = async () => {
+    const html = generateHTML(roles);
+    writeToFile("index.html", html);
+
+    const answers = await inquirer.prompt(questions);
+};
+
+init();
--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,11 +1,33 @@
+const Employee = require("../src/lib/Employee");
+
 describe("Employee Class", () => {
-  test("should be an instance of Employee", () => { });
+  const mockEmployee = {
+    name: "Jane Smith",
+    id: 1553445,
+    email: "hello@hop.com",
+  };
+  const actual = new Employee(mockEmployee);
 
-  test("should return the expected name", () => { });
- 
-  test("should return the expected id", () => { });
-  
-  test("should return the expected email", () => { });
+  test("should be an instance of Employee", () => {
+    const actual = new Employee(mockEmployee);
+    expect(actual).toBeInstanceOf(Employee);
+  });
 
-  test("should return the ", () => { })
-};
+  test("should return the expected name", () => {
+    const name = actual.getName();
+    const expected = mockEmployee.name;
+    expect(name).toEqual(expected);
+  });
+
+  test("should return the expected id", () => {
+    const id = actual.getId();
+    const expected = mockEmployee.id;
+    expect(id).toEqual(expected);
+  });
+
+  test("should return the expected email", () => {
+    const email = actual.getEmail();
+    const expected = mockEmployee.email;
+    expect(email).toEqual(expected);
+  });
+});
