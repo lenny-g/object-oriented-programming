f4f81db1a47fe248742618f12fe57671061711bd
lenny-g <leanne_gallag@live.co.uk>
2021-11-24 20:30:36 +0000

    question index

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,63 +1,71 @@
 const inquirer = require("inquirer");
+// const { Employee, Manager, Engineer, Intern } = require(".lib");
 const fs = require("fs");
 
-const mainQuestions = [
-    {
-       type: "list",
-       name: "role",
-       message: "What is your role?",
-          choices: [
-              { name: "Manager", value: "manager"},
-              { name: "Engineer", value: "engineer"},
-              { name: "Intern", value: "intern" }
-          ]
-      },
-    {
-      type: "input",
-      name: "name",
-      message: "What is your name?",
-    },
-    {
-      type: "number",
-      name: "id",
-      message: "What is your ID? ",
-    },
-  
-    {
-      type: "input",
-      name: "email",
-      message: "What is your email?",
-    }, ]
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?",
+  },
+  {
+    type: "number",
+    name: "id",
+    message: "What is your id?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  },
+  {
+    type: "number",
+    name: "officeNumber",
+    message: "What is your office number?",
+  },
+  {
+    type: "list",
+    name: "role",
+    message: "What is your role?",
+    choices: [
+      { name: "Manager", value: "manager" },
+      { name: "Engineer", value: "engineer" },
+      { name: "Intern", value: "intern" },
+    ],
+  },
+  {
+    type: "number",
+    name: "officeNumber",
+    message: "What is your office number?",
+    when: (role) => role === "manager",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your github username?",
+    when: (role) => role === "engineer",
+  },
 
+  {
+    type: "input",
+    name: "school",
+    message: "What is the name of your school?",
+    when: (role) => role === "intern",
+  },
+];
 
-managerQuestion = [{
-      
-    }]
-
-const generateHTML = (answers) => {
-    const {
-        role,
-        name,
-        id,
-        email,
-        officeNumber,
-        github,
-        school
-}
-}
-
-const writeToFile = (fileName, data) => {
-    fs.writeFile(fileName, data, (err) =>
-      err ? console.error(err) : console.log("Success!")
-    );
-};
-  
+// const writeToFile = (fileName, data) => {
+//   fs.writeFile(fileName, data, (err) =>
+//     err ? console.error(err) : console.log("Success!")
+//   );
+// };
 
 const init = async () => {
-    const html = generateHTML(roles);
-    writeToFile("index.html", html);
+  // const html = generateHTML(answers);
+  // writeToFile("index.html", html);
 
-    const answers = await inquirer.prompt(questions);
+  const answers = await inquirer.prompt(questions);
+  console.log(answers);
 };
 
 init();
